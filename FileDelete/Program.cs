f5c340c6141b29152118dc1f994945cfaf98b041
edabydr <120217448+edabydr@users.noreply.github.com>
2022-12-14 18:59:54 +0300

    Belirli tarih sonrası oluşan dosyaları otomatik olarak silme
    Stajyer olarak çalıştığım yerde projeye eklenti yaptım

---
+++ FileDelete/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace FileDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var originalDirectory = new DirectoryInfo(@"C:\\sil\\");
+            string pathString = originalDirectory.ToString();
+            DirectoryInfo fileinfo = new DirectoryInfo(pathString);
+            foreach (FileInfo item in fileinfo.GetFiles("*.txt"))
+            {
+                //gün değişmişse dosyayı al ve sil
+                if((DateTime.Now - item.CreationTime  ).TotalSeconds >= 1)
+                {
+                    File.Delete(item.FullName);
+                }
+            }
+         
+        }
+        
+    }
+}
+
